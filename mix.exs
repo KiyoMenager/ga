757df4b90b27e6f665125a9634e13f3255ac4ae6
Kiyo <menager.kiyoaki@gmail.com>
2016-11-20 23:41:34 +0100

    Extract problem

--- mix.exs
+++ mix.exs
@@ -4,7 +4,11 @@ defmodule Ga.Mixfile do
   def project do
     [app: :ga,
      version: "0.1.0",
-     elixir: "~> 1.3",
+     build_path: "../../_build",
+     config_path: "../../config/config.exs",
+     deps_path: "../../deps",
+     lockfile: "../../mix.lock",
+     elixir: "~> 1.3.2",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
      deps: deps()]
@@ -28,9 +32,9 @@ defmodule Ga.Mixfile do
   # Type "mix help deps" for more examples and options
   defp deps do
     [
-      {:dataset_loader, git: "https://github.com/KiyoMenager/dataset_loader.git"},
+      {:problem, git: "https://github.com/KiyoMenager/problem.git"},
       {:toroidal_grid, git: "https://github.com/KiyoMenager/toroidal_grid.git"},
-      {:distance_matrix, git: "https://github.com/KiyoMenager/distance_matrix.git"}
+      # {:distance_matrix, git: "https://github.com/KiyoMenager/distance_matrix.git"}
     ]
   end
 end
